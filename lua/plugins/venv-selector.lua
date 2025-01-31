d1a2e0e22a89951bdb66b4d8d032323ef812d828
huangguoyu.hgy <huangguoyu.hgy@outlook.com>
2025-01-31 22:09:52 +0800

    feat: 开启python虚拟环境调试支持

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -6,6 +6,7 @@ return {
       -- Your options go here
       -- name = "venv",
       auto_refresh = true,
+      dap_enabled = true,
     })
   end,
   event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
